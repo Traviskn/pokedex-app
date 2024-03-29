855a6dc0861a99741149f38322bf0090e458e7d0
Travis Nuttall <tuttall@gmail.com>
2024-03-28 22:45:16 -0600

    track current location and display on a map

--- screens/Go.tsx
+++ screens/Go.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+import MapView, { Marker, Region } from "react-native-maps";
+import * as Location from "expo-location";
+import { View } from "react-native";
+
+const latitudeDelta = 0.0922;
+const longitudeDelta = 0.0421;
+
+const initialRegion = {
+  latitude: 0,
+  longitude: 0,
+  latitudeDelta,
+  longitudeDelta,
+};
+
 export default function Go() {
-  return null;
+  const [region, setRegion] = useState<Region>(initialRegion);
+
+  const [status, requestPermission] = Location.useForegroundPermissions();
+
+  useEffect(() => {
+    if (status?.status === "undetermined") {
+      requestPermission();
+    } else if (status?.status === "granted") {
+      const locationSubPromise = Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 500,
+        },
+        (location) => {
+          setRegion({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta,
+            longitudeDelta,
+          });
+        },
+      );
+      return () => {
+        locationSubPromise.then((sub) => sub.remove());
+      };
+    }
+  }, [status]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView region={region} style={{ height: "100%", width: "100%" }}>
+        <Marker key="trainer" coordinate={region} />
+      </MapView>
+    </View>
+  );
 }
